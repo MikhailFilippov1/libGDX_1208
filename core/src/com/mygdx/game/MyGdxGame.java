d6f734d7ee1b6738d886d62934a05ec8e86fb702
MikhailFilippov1 <cp_filippov@mail.ru>
2022-08-24 15:02:29 +0300

    Здравствуйте, Евгений. Более-менее оклемался. Высылаю ДЗ2. Персонаж ходит от рамки до рамки и поворачивает по кнопкам. Короче, пока без особых изысков. Буду нагонять группу, а там посмотрим.

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -4,39 +4,64 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
 	int clk;
-	
+	AnimationHero animationHero;
+	boolean heroDirection = true;
+	boolean lookRight = true;
+	int distance = 0;
+	int heroPositionX = 0;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("drop.png");
+
+		animationHero = new AnimationHero("elemental.png", 8, 1);
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(0.5f, 0.5f, 0.5f, 1);
 
-		float x = Gdx.input.getX() - img.getWidth()/2;
-		float y = Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2;
+		animationHero.setTime(Gdx.graphics.getDeltaTime());
+		float x = Gdx.input.getX() - animationHero.getFrame().getRegionWidth()/2;
+		float y = Gdx.graphics.getHeight() - Gdx.input.getY() - animationHero.getFrame().getRegionHeight()/2;
 
 		if(Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) clk++;
 		Gdx.graphics.setTitle("Clicks " + clk + " times!");
 
+		if(Gdx.input.isKeyJustPressed(Input.Keys.L)) heroDirection = true;
+		if(Gdx.input.isKeyJustPressed(Input.Keys.R)) heroDirection = false;
+
+		if(Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) lookRight = false;
+		if(Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) lookRight = true;
+		if(heroPositionX + 128 >= Gdx.graphics.getWidth()) lookRight = false;
+		if(heroPositionX <= 0)lookRight = true;
+
+		if(!animationHero.getFrame().isFlipX() && !lookRight) animationHero.getFrame().flip(true, false);
+		if(animationHero.getFrame().isFlipX() && lookRight) animationHero.getFrame().flip(true, false);
+
+		if(lookRight) {
+			heroPositionX += 1;
+		}
+		else  {
+			heroPositionX -= 1;
+		}
+
 		batch.begin();
-		//batch.draw(img, 0, 0);  // убрал их картинку
-		batch.draw(img, x, y);
+		batch.draw(animationHero.getFrame(), heroPositionX, 0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		animationHero.dispose();
 	}
 }
